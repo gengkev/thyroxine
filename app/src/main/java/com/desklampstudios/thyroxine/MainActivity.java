c6decf724020fff59047ab906b8fcb94d62915af
Kevin Geng <gengkev@gmail.com>
2014-11-28 02:00:40 -0500

    replaced code for fragment
    Following the Android guide for creating a navigation drawer, rather
    than the default code generated by Android Studio. I felt all the code
    in the NavigationDrawerFragment class was too complex, especially since
    changing the action bar isn't necessary anymore with the Material Action
    Bar.
    Of course, the MainActivity class is now a bit verbose.

--- app/src/main/java/com/desklampstudios/thyroxine/MainActivity.java
+++ app/src/main/java/com/desklampstudios/thyroxine/MainActivity.java
@@ -5,10 +5,12 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,18 +18,29 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.desklampstudios.thyroxine.eighth.EighthFragment;
 import com.desklampstudios.thyroxine.news.NewsFragment;
 
 
-public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends ActionBarActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final String PREFS_NAME = "SessionPrefs";
+    private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
 
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-    private CharSequence mTitle; // Last title for restoreActionBar()
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private View mDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private int mDrawerSelectedPosition = 0;
+    private String[] mNavTitles = new String[] {
+            "News", "Eighth"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +51,10 @@ public class MainActivity extends ActionBarActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // load savedInstanceState
+        if (savedInstanceState != null) {
+            mDrawerSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
+        }
 
         // Load cookies?
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
@@ -46,67 +63,82 @@ public class MainActivity extends ActionBarActivity
         Log.d(TAG, "Retrieving cookies: " + cookies);
         IodineApiHelper.setCookies(cookies);
 
+        // Navigation Drawer
+        mNavTitles = getResources().getStringArray(R.array.nav_titles);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer = findViewById(R.id.left_drawer);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer_list);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+        // create drawer toggle
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        // create ArrayAdapter to display drawer items, add click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(
+                getSupportActionBar().getThemedContext(),
+                android.R.layout.simple_list_item_activated_1,
+                android.R.id.text1,
+                mNavTitles));
+
+        mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
+
+        // select an item in drawer
+        selectItem(mDrawerSelectedPosition);
     }
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        mDrawerSelectedPosition = position;
 
         Fragment fragment;
         switch (position) {
             case 0:
-                mTitle = getString(R.string.title_activity_news);
                 fragment = NewsFragment.newInstance(false);
                 break;
             case 1:
-                mTitle = getString(R.string.title_activity_eighth);
                 fragment = EighthFragment.newInstance(true);
                 break;
-            case 2:
-                mTitle = "Section 3";
-                fragment = PlaceholderFragment.newInstance(3);
-                break;
             default:
-                throw new IllegalArgumentException();
+                fragment = PlaceholderFragment.newInstance(
+                        mNavTitles[position] + " (placeholder)");
+                break;
         }
 
+        // Insert the fragment by replacing any existing fragment
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.container, fragment)
                 .commit();
-    }
 
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mNavTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawer);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // If the drawer toggle handles it, it will return true
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         switch (item.getItemId()) {
             case R.id.action_login:
                 Intent intent = new Intent(this, LoginActivity.class);
@@ -117,12 +149,30 @@ public class MainActivity extends ActionBarActivity
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save drawer state
+        outState.putInt(STATE_SELECTED_POSITION, mDrawerSelectedPosition);
+    }
+
+    @Override
     public void onBackPressed() {
         // Close drawer on back button press
-        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        View drawer = findViewById(R.id.navigation_drawer);
-        if (drawerLayout != null && drawerLayout.isDrawerOpen(drawer)) {
-            drawerLayout.closeDrawer(drawer);
+        if (mDrawerLayout.isDrawerOpen(mDrawer)) {
+            mDrawerLayout.closeDrawer(mDrawer);
             return;
         }
         super.onBackPressed();
@@ -132,23 +182,15 @@ public class MainActivity extends ActionBarActivity
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        private static final String ARG_TITLE = "title";
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(String title) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putString(ARG_TITLE, title);
             fragment.setArguments(args);
             return fragment;
         }
@@ -157,13 +199,12 @@ public class MainActivity extends ActionBarActivity
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+
+            String title = getArguments().getString(ARG_TITLE);
+            textView.setText(title);
+
             return rootView;
         }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-        }
     }
-
 }
